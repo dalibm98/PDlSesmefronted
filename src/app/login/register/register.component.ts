c4bf51030b1db0cee7f38db76a16eec17bbe2c18
dali <74265428+dalibm98@users.noreply.github.com>
2023-04-15 11:23:03 +0100

    message login

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -6,23 +6,24 @@ import { AuthenticationService } from 'src/app/service/authentication.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-
-
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
   loading: boolean = false;
   showSuccessMessage: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthenticationService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService
+  ) {}
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
@@ -35,7 +36,7 @@ export class RegisterComponent implements OnInit {
       firstname: this.registerForm.controls['firstname'].value,
       lastname: this.registerForm.controls['lastname'].value,
       email: this.registerForm.controls['email'].value,
-      password: this.registerForm.controls['password'].value
+      password: this.registerForm.controls['password'].value,
     };
 
     this.loading = true; // Afficher le spinner
